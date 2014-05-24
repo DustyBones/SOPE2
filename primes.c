341da6d8537a31f18a41c5b43087d0d1acc384f1
Tiago Figueiredo <tiago.fa.figueiredo@gmail.com>
2014-05-24 22:16:15 +0100

    done

--- primes.c
+++ primes.c
@@ -1,33 +1,29 @@
-
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <math.h>
 #include <pthread.h> 
 #include <semaphore.h>
 
-#define NOT_SHARED 0
-#define N_EMLEM(x) (sizeof(x)/sizeof(x[0]))
-
- sem_t done;
- pthread_mutex_t mut=PTHREAD_MUTEX_INITIALIZER;
- int *primes, *numPrimes, *N;
-//------------------------------------------------------------------------------------------ 
-// Type of the circular queue elements 
+ pthread_mutex_t threadMutex=PTHREAD_MUTEX_INITIALIZER;	//thread related mutex
+ pthread_t* threads;									//list of threads
+ int numThreads;										//number of threads
+ pthread_mutex_t primeMutex=PTHREAD_MUTEX_INITIALIZER;	//prime related mutex
+ int* primes;											//list of primes
+ int numPrimes;											//number of primes
  
-typedef unsigned long QueueElem; 
+ int N; 												//list size
+ sem_t done;											//semaphore, done
+ 										
+ typedef unsigned long QueueElem;  						// Type of the circular queue elements 
  
-//------------------------------------------------------------------------------------------ 
-// Struct for representing a "circular queue" 
-// Space for the queue elements will be allocated dinamically by queue_init() 
- 
-typedef struct { 
- QueueElem *v; // pointer to the queue buffer unsigned int capacity;
- unsigned int capacity; // queue capacity
- unsigned int first; // head of the queue 
- unsigned int last; // tail of the queue 
- sem_t empty; // semaphores and mutex for implementing the 
- sem_t full; // producer-consumer paradigm pthread_mutex_t mutex; 
- pthread_mutex_t mutex; 
+ typedef struct { 										// Struct for representing a "circular queue" 
+	QueueElem *v;										// pointer to the queue buffer unsigned int capacity;
+	unsigned int capacity;				 				// queue capacity
+	unsigned int first; 								// head of the queue 
+	unsigned int last; 									// tail of the queue 
+	sem_t empty;										// semaphores and mutex for implementing the 
+	sem_t full; 										// producer-consumer paradigm pthread_mutex_t mutex; 
+	pthread_mutex_t mutex; 
 } CircularQueue; 
  
 //------------------------------------------------------------------------------------------ 
@@ -37,27 +33,26 @@ typedef struct {
 // TO DO BY STUDENTS: ADD ERROR TESTS TO THE CALLS & RETURN a value INDICATING (UN)SUCESS 
  
 void queue_init(CircularQueue **q, unsigned int capacity){ // TO DO: change return value { 
- *q = (CircularQueue *) malloc(sizeof(CircularQueue)); 
- sem_init(&((*q)->empty), 0, capacity); 
- sem_init(&((*q)->full), 0, 0); 
- pthread_mutex_init(&((*q)->mutex), NULL); 
- (*q)->v = (QueueElem *) malloc(capacity * sizeof(QueueElem)); 
- (*q)->capacity = capacity; 
- (*q)->first = 0; 
- (*q)->last = 0; 
+	*q = (CircularQueue *) malloc(sizeof(CircularQueue)); 
+	sem_init(&((*q)->empty), 0, capacity); 
+	sem_init(&((*q)->full), 0, 0); 
+	pthread_mutex_init(&((*q)->mutex), NULL); 
+	(*q)->v = (QueueElem *) malloc(capacity * sizeof(QueueElem)); 
+	(*q)->capacity = capacity; 
+	(*q)->first = 0; 
+	(*q)->last = 0; 
 } 
  
 //------------------------------------------------------------------------------------------ 
 // Inserts 'value' at the tail of queue 'q' 
  
 void queue_put(CircularQueue *q, QueueElem value) { 
-	sem_wait(&(q->empty));
-	pthread_mutex_lock(&(q->mutex));
+	sem_wait(&q->empty);
+	pthread_mutex_lock(&q->mutex);
 	q->v[q->last]=value;
-	(q->last)++;
-	(q->last)%=(q->capacity);
-	pthread_mutex_unlock(&(q->mutex));
-	sem_post(&(q->full));
+	q->last=(q->last+1)%(q->capacity);
+	pthread_mutex_unlock(&q->mutex);
+	sem_post(&q->full);
 	return;
 } 
  
@@ -65,14 +60,13 @@ void queue_put(CircularQueue *q, QueueElem value) {
 // Removes element at the head of queue 'q' and returns its 'value' 
  
 QueueElem queue_get(CircularQueue *q) {
-	QueueElem value=0;
-	sem_wait(&(q->full));
+	QueueElem value;
+	sem_wait(&q->full);
 	pthread_mutex_lock(&(q->mutex));
 	value=q->v[q->first];
-	(q->first)++;
-	(q->first)%=(q->capacity);
-	pthread_mutex_unlock(&(q->mutex));
-	sem_post(&(q->empty));
+	q->first=(q->first+1)%(q->capacity);
+	pthread_mutex_unlock(&q->mutex);
+	sem_post(&q->empty);
 	return value;
 } 
  
@@ -87,62 +81,64 @@ void queue_destroy(CircularQueue *q) {
 } 
 
 void *processer (void *q){
-	pthread_t pid;
-	int temp, prime;
 	
+	QueueElem prime, remainder;
 	prime=queue_get(q);
-	
-	if(prime>sqrt(*N)){
-		while(prime>0){
-			pthread_mutex_lock(&mut);
-			primes[*numPrimes]=prime;
-			(*numPrimes)++;
-			pthread_mutex_unlock(&mut);
-			prime=queue_get(q);
+	remainder=queue_get(q);
+	if(prime>sqrt(N)){
+		while(remainder!=0){
+			pthread_mutex_lock(&primeMutex);
+			primes[numPrimes]=remainder;
+			(numPrimes)++;
+			pthread_mutex_unlock(&primeMutex);
+			remainder=queue_get(q);
 		}
 		queue_destroy(q);
 		sem_post(&done);
 	}else{
 		CircularQueue* newData;
 		queue_init(&newData, 10);
-		pthread_create(&pid, NULL, processer, newData);
-		temp=prime;
-		while(temp>0){
-			temp=queue_get(q);
-			if(temp%prime!=0){
-				queue_put(newData, temp);
+		pthread_mutex_lock(&threadMutex);
+		pthread_create(&threads[numThreads], NULL, processer, newData);
+		numThreads++;
+		pthread_mutex_unlock(&threadMutex);
+		while(remainder!=0){
+			if(remainder!=0){
+				if(remainder%prime!=0){
+					queue_put(newData, remainder);
+				}
 			}
+			remainder=queue_get(q);
 		}
 		queue_put(newData, 0);
 		queue_destroy(q);
-		pthread_mutex_lock(&mut);
-		primes[*numPrimes]=prime;
-		(*numPrimes)++;
-		pthread_mutex_unlock(&mut);
-		pthread_join(pid, NULL); 
 	}
+	pthread_mutex_lock(&primeMutex);
+	primes[numPrimes]=prime;
+	numPrimes++;
+	pthread_mutex_unlock(&primeMutex);
+	
 	return NULL;
 }
 
 void *starter (void *arg){
-	pthread_t pid;
 	
-	primes[*numPrimes]=2;
-	(*numPrimes)++;
-	if (2<*N){
+	primes[numPrimes]=2;
+	numPrimes++;
+	
+	if (2<N){
 		CircularQueue* data;
 		queue_init(&data, 10);
-		pthread_create(&pid, NULL, processer, data);
+		pthread_mutex_lock(&threadMutex);
+		pthread_create(&threads[numThreads], NULL, processer, data);
+		numThreads++;
+		pthread_mutex_unlock(&threadMutex);
 		int i;
-		for(i=3; i<=*N; ++i){
-			if(i%2!=0){
-				queue_put(data, i);
-			}
+		for(i=3; i<=N; i+=2){
+			queue_put(data, i);
 		}
 		queue_put(data, 0);
-		pthread_join(pid, NULL); 
-	}
-	sem_post(&done);
+	}else sem_post(&done);
 	return NULL;
 }
 
@@ -151,41 +147,44 @@ int cmpfunc (const void * a, const void * b){
 }
  
  int main(int argc, char *argv[]) { 
-	pthread_t sid; 
 	
 	if (argc != 2 || atoi(argv[1])<2) { 
 		fprintf(stderr,"USAGE: %s numItems [numItems >= 2]\n",argv[0]); 
 		exit(1); 
 	} 
+	
 	//allocate shared memory
-	N = (int *)malloc(sizeof(int));
-	*N=atoi(argv[1]);
-	primes = (int *)malloc(1.2*(*N)/log(*N));
-	numPrimes = (int *)malloc(sizeof(int));
-	*numPrimes=0;
+	N=atoi(argv[1]);
+	primes = (int *)malloc(ceil(1.2*(N)/log(N))*sizeof(int));
+	numPrimes=0;
+	threads = (pthread_t *)malloc(ceil(sqrt(N))*sizeof(pthread_t));
+	numThreads=0;
+	
 	
 	//initialize the semaphore
-	sem_init(&done, NOT_SHARED, 0);
+	sem_init(&done, 0, 0);
 	
 	//launch the threads
-	pthread_create(&sid, NULL, starter, &N); 
+	pthread_mutex_lock(&threadMutex);
+	pthread_create(&threads[numThreads], NULL, starter, &N);
+	numThreads++;
+	pthread_mutex_unlock(&threadMutex);
+	
+	//wait untill everything is finished
 	sem_wait(&done);
- 
-	sem_destroy(&done); 
-	pthread_join(sid, NULL); 
+	sem_destroy(&done);
+	int i;
+	for(i=0; i<numThreads;++i){
+		pthread_join(threads[i], NULL);
+	}
 
 	//show the results
-	qsort(primes, *numPrimes, sizeof(int), cmpfunc);
-	int i;
-	printf("Primes between 2 and %d:\n1. %d\n", *N, primes[0]);
-	for(i=1; i<*numPrimes; ++i){
-		printf("%d. %d\n",i+1, primes[i]);
+	qsort(primes, numPrimes, sizeof(int), cmpfunc);
+	int j;
+	printf("Primes between 2 and %d: %d\n", N, numPrimes);
+	for(j=0; j<numPrimes; ++j){
+		printf("%d. %d\n",j+1, primes[j]);
 	}
-	
-	//release shared memory
-	//free(primes);
-	//free(numPrimes);
-	//free(N);
 	
 	return 0;
 }
